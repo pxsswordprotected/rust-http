df64ae66ea538d1d24b0d6e57cbd8bf3f28a4297
pxsswordprotected <jamesalford661@gmail.com>
2025-01-12 10:58:37 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::net::TcpListener;
+
+fn main() {
+
+    
+
+
+    // Your code here:
+    // 1. Create a TcpListener that binds to "127.0.0.1:7878"
+    // 2. Use a for loop to iterate over incoming connections
+    // 3. Print something when a connection is received}
+}
